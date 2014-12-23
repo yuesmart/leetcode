b4bb053e16160b3a8c3ef9ebac3b2da38f053df1
Martin Xu <martin.xus@gmail.com>
2014-12-23 14:14:46 +0800

    add null return

--- ruby/01_two_sum.rb
+++ ruby/01_two_sum.rb
@@ -8,11 +8,11 @@ def two_sum(numbers,target)
     index1 = index1 + 1
     numbers[index1..numbers.length].each_with_index do |item2,index2|
       index2 = index2 + 1
-      if item1 + item2 == target
-        return [index1,index1+index2]
-      end
+      return [index1,index1+index2] if item1 + item2 == target        
     end
   end
+  
+  [nil,nil]
 end
 
-puts "two_sum([2,7,11,15],9) => #{two_sum([2,7,11,15],9)}"
+puts "two_sum([2,8,11,15],9) => #{two_sum([2,8,11,15],9)}"
